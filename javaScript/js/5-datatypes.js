ccaba36506b4aa2093e72a87ae62e35b1d0ef6f0
AnJaWr <october3@o2.pl>
2019-04-27 11:52:57 +0200

    Kolejne zadania

---
+++ javaScript/js/5-datatypes.js
@@ -0,0 +1,50 @@
+'use strict';
+
+// typ liczbowy
+
+let wyplata = 3500;
+let premia = 1000;
+let calkowitaWyplata;
+calkowitaWyplata = wyplata + premia;
+
+console.log(calkowitaWyplata);
+console.log(typeof calkowitaWyplata);
+
+// typ string
+
+let wyplataStr = "3500";
+let premiaStr = "1000";
+let calkowitaWyplataStr;
+calkowitaWyplataStr = wyplataStr + premiaStr;
+
+console.log(calkowitaWyplataStr);
+console.log(typeof calkowitaWyplataStr);
+
+// ES 6 !!!
+let nazwisko = "Kroszka";
+let student = `Michal ${nazwisko}`;
+console.log(student);
+
+
+// zmienne logiczne
+
+let czyJestSmog;
+czyJestSmog = true;
+
+
+
+
+console.log(niezdefiniowanaZmienna);
+console.log(nullowaZmienna);
+
+// typy spejalne - undefined / null
+
+let niezdefiniowanaZmienna;
+let nullowaZmienna = null;
+
+console.log(niezdefiniowanaZmienna);
+console.log(nullowaZmienna);
+
+// TYPY REFERENCYJNE
+
+// tablice
