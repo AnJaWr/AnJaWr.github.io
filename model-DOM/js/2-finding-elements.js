b4cd1178a56362e38a4f07c4c19afbc2b7cea949
AnJaWr <october3@o2.pl>
2019-04-28 09:57:48 +0200

    znajdowanie elementu

---
+++ model-DOM/js/2-finding-elements.js
@@ -0,0 +1,30 @@
+'use strict';
+
+let parFirst = document.getElementById('parFirst');
+console.log(parFirst);
+
+let linki = document.getElementsByClassName('superlink');
+console.log(linki);
+
+let linkiPoTagu = document.getElementsByTagName('a');
+console.log(linkiPoTagu);
+
+
+let divPoId = document.querySelectorAll('#parSeond');
+console.log(divPoId);
+
+let pierwszyLinkPoSelektorze = document.querySelector('.superlink');
+console.log(pierwszyLinkPoSelektorze);
+// querySeletor pobiera pierwszy selektor!
+
+let linkiPoSelektorze = document.querySelectorAll('.superlink');
+console.log(linkiPoSelektorze);
+// querySeletor pobiera pierwszy selektor!
+
+linkiPoSelektorze.forEach((link, i) => {
+    console.log(link.outerHTML);
+});
+
+// tworzymy zmienną let link... -> pobieramy element (doument.query...) consola-> node list
+// chemy przeleciec wszytskie elementy,zmieni style zatem: ement. 
+// linki - wrzuci pierwszy element - i - kolejne elementy indexu
