c8d10006981ee89646650d943fc4916bce1530f5
AnJaWr <october3@o2.pl>
2019-05-03 14:47:34 +0200

    formularz - kolejna próba

--- model-DOM-zadania/walidacja-formularz/js/script.js
+++ model-DOM-zadania/walidacja-formularz/js/script.js
@@ -1,58 +1,65 @@
 'use strict';
-/* Opcja zaznaczania checboxów */
-
+/* Przypisanie*/
+const formularz = document.querySelector('form');
+const polaTekstowe = document.querySelectorAll('input[type=text]');
 const zgoda1 = document.getElementById('zgoda-marketingowa-1');
 
 const zgoda2 = document.getElementById('zgoda-marketingowa-2');
 
-const zgody = document.getElementById("wszystkie-zgody");
+const zgody = document.getElementById('wszystkie-zgody');
 
+/* Zaznaczanie wszystkich zgód na raz */
 zgody.addEventListener("change", zaznaczWszystkie);
 
 function zaznaczWszystkie() {
 
+
     if (zgody.checked) {
         zgoda1.checked = true;
         zgoda2.checked = true;
+        zgoda1.disabled = true;
+        zgoda2.disabled = true;
     } else {
         zgoda1.checked = false;
         zgoda2.checked = false;
         zgoda1.disabled = false;
         zgoda2.disabled = false;
     }
-}
-
-/* Walidacja */
-
-const formularz = document.querySelector('form');
+};
+/* WALIDACJA */
 
 
-const walidacja = (event) => {
 
-    const polaTekstowe = document.querySelectorAll('input[type=text]');
+formularz.addEventListener('submit', walidacja);
 
+function walidacja(e) {
+    e.preventDefault();
     for (let i = 0; i < polaTekstowe.length; i++) {
+        // dla każdego inputu typu text sprawdź warunek:
         if (polaTekstowe[i].value == "") {
-            event.preventDefault();
+            // jeżeli string jest pusty
+
+            // nie wysyłaj
+            e.preventDefault();
+            //  tworzymy wyskakujacy jako paragraf błąd:
             let error1 = document.createElement("p");
+            // ustawiamy tekst, wpisany w ten paragraf
             error1.innerHTML = "Uzupełnij dane";
-            polaTekstowe[i].parentNode.replaceChild(error1, polaTekstowe[i].nextSibling);
+            //każemy mu się wstawić pod polem:
+            formularz.insertBefore(error1, polaTekstowe[i]);
         } else {
-            error1.remove();
+            console.log("Warunek spełniony");
+            formularz.removeChild(error1);
         }
-
     }
-
     if (zgoda1.checked == false) {
         event.preventDefault();
         let error2 = document.createElement("p");
         error2.innerHTML = "Zaznacz zgodę";
-        zgoda1.parentNode.replaceChild(error2, zgoda1.nextSibling);
+        formularz.insertBefore(error2, zgoda2);
     } else {
-        error2.remove();
+        console.log("Warunek spełniony");
+        formularz.removeChild(error2);
     }
 
-}
-
-
-formularz.addEventListener('submit', walidacja)
+};
