0208db3992214336dd65bc1989726b8749b5ccd3
AnJaWr <october3@o2.pl>
2019-05-11 10:17:13 +0200

    jQuery zadania

---
+++ jQuery/cwiczenia/js/3-dom-manipulacja.js
@@ -0,0 +1,12 @@
+let paragrafy = $('p');
+// console.log(paragrafy.text());
+
+
+// paragrafy.text('Nowy tekst');
+
+
+let paragrafyByHtml = $('p');
+console.log(paragrafyByHtml.html());
+
+// zwroci tylko z pirrwszego
+paragrafyByHtml.html('<span>Treść znacznika span</span>');
