9ed195860c2529a5a5078319e69a5ced184639d8
AnJaWr <october3@o2.pl>
2019-05-28 22:15:06 +0200

    dodaję local storage

--- rekrutacja/js/todolist.js
+++ rekrutacja/js/todolist.js
@@ -1,22 +1,28 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    $('#toDoList').html(localStorage.getItem('listItems'));
 
-    $("#add").click(function() {
+    $("#add").click(function () {
         let newListItem = $("#addtoList").val();
         let counter = $('li.item').length;
         if (newListItem.length > 0) {
             counter++;
             $("#toDoList").append("<li class='item'>" + "Task number " + counter + ": " + newListItem + "</li>");
+            localStorage.setItem('listItems', $('#toDoList').html());
             $("#addtoList").val('');
         }
+
     });
 
-    $("#remove").click(function() {
+
+    $("#remove").click(function () {
         $("#toDoList li:last-child").remove();
+        localStorage.setItem('listItems', $('#toDoList').html());
     });
 
 
-    $("#clear").click(function() {
+    $("#clear").click(function () {
         $("#toDoList li").remove();
+        localStorage.setItem('listItems', $('#toDoList').html());
     });
 
 });
