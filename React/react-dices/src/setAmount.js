7056a56d2ead44dcb483f19f5d9eb54125755748
AnJaWr <october3@o2.pl>
2019-06-30 11:43:00 +0200

    Nadal znika wynik

--- React/react-dices/src/setAmount.js
+++ React/react-dices/src/setAmount.js
@@ -6,9 +6,9 @@ class SetAmount extends Component {
 
 
     setAmount = (e) => {
-        
+
         this.props.setCounter(parseInt(this.dicesAmount.value))
-e.preventDefault()
+        e.preventDefault()
     }
 
 
@@ -16,7 +16,7 @@ e.preventDefault()
     render() {
         return (
             <div>
-                <form >
+                <form onSumbit={(e) => e.preventDefault()}>
                     <label htmlFor="number" > Ilość kości:</label>
                     <input name="number" type="number" ref={input => this.dicesAmount = input} min="1" max="12" placeholder="0" />
                     <input type="submit" value="zatwierdź" onClick={() => { this.setAmount() }} />
