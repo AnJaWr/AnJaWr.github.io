cbdda70bb49ddd6dfbb9701ce955ba68139340af
AnJaWr <october3@o2.pl>
2019-06-30 15:01:47 +0200

    Clear

--- Bootstrap/warsztaty/main/script.js
+++
@@ -1,2 +0,0 @@
-const iframe = document.querySelector("iframe");
-iframe.style.height = iframe.getBoundingClientRect().width / 1.777 + "px";
--- javaScript/js/8-obiekty-math.js
+++
@@ -1,2 +0,0 @@
-console.log(Math.PI);
-console.log(Math.cos(0));
