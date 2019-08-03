5e4eaa973ea749c6febb4c282fd96a48466fd7c6
AnJaWr <october3@o2.pl>
2019-08-03 17:09:31 +0200

    scripts changed

--- my_portfolio/js/mainn.js
+++ my_portfolio/js/mainn.js
@@ -23,7 +23,6 @@
 		}
 	};
 
-
 	var fullHeight = function () {
 
 		if (!isMobile.any()) {
@@ -33,11 +32,50 @@
 			});
 		}
 	};
-
-	// Parallax
+//parallax
 	var parallax = function () {
 		$(window).stellar();
 	};
+
+	var contentWayPoint = function () {
+		var i = 0;
+		$('.animate-box').waypoint(function (direction) {
+
+			if (direction === 'down' && !$(this.element).hasClass('animated-fast')) {
+
+				i++;
+
+				$(this.element).addClass('item-animate');
+				setTimeout(function () {
+
+					$('body .animate-box.item-animate').each(function (k) {
+						var el = $(this);
+						setTimeout(function () {
+							var effect = el.data('animate-effect');
+							if (effect === 'fadeIn') {
+								el.addClass('fadeIn animated-fast');
+							} else if (effect === 'fadeInLeft') {
+								el.addClass('fadeInLeft animated-fast');
+							} else if (effect === 'fadeInRight') {
+								el.addClass('fadeInRight animated-fast');
+							} else {
+								el.addClass('fadeInUp animated-fast');
+							}
+
+							el.removeClass('item-animate');
+						}, k * 100, 'easeInOutExpo');
+					});
+
+				}, 50);
+
+			}
+
+		}, { offset: '85%' });
+	};
+
+
+
+
 
 	var contentWayPoint = function () {
 		var i = 0;
@@ -103,33 +141,9 @@
 
 	};
 
-	var pieChart = function () {
-		$('.chart').easyPieChart({
-			scaleColor: false,
-			lineWidth: 4,
-			lineCap: 'butt',
-			barColor: '#FF9000',
-			trackColor: "#f5f5f5",
-			size: 160,
-			animate: 1000
-		});
-	};
-
-	var skillsWayPoint = function () {
-		if ($('#my_portfolio-skills').length > 0) {
-			$('#my_portfolio-skills').waypoint(function (direction) {
-
-				if (direction === 'down' && !$(this.element).hasClass('animated')) {
-					setTimeout(pieChart, 400);
-					$(this.element).addClass('animated');
-				}
-			}, { offset: '90%' });
-		}
-
-	};
 
 
-	// Loading page
+
 	var loaderPage = function () {
 		$(".my_portfolio-loader").fadeOut("slow");
 	};
@@ -139,10 +153,10 @@
 		contentWayPoint();
 		goToTop();
 		loaderPage();
-		fullHeight();
 		parallax();
-		// pieChart();
+		fullHeight();
 		skillsWayPoint();
+
 	});
 
 
