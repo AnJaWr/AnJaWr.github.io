19f376781538ef3c92d83f0a34ea21e109dd1377
AnJaWr <october3@o2.pl>
2020-03-27 15:00:27 +0100

    artstation button added

--- my_portfolio/js/main.js
+++ my_portfolio/js/main.js
@@ -1,157 +1,166 @@
-; (function () {
+(function() {
+  'use strict';
 
-	'use strict';
+  var isMobile = {
+    Android: function() {
+      return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+      return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+      return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+      return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+      return (
+        isMobile.Android() ||
+        isMobile.BlackBerry() ||
+        isMobile.iOS() ||
+        isMobile.Opera() ||
+        isMobile.Windows()
+      );
+    }
+  };
 
-	var isMobile = {
-		Android: function () {
-			return navigator.userAgent.match(/Android/i);
-		},
-		BlackBerry: function () {
-			return navigator.userAgent.match(/BlackBerry/i);
-		},
-		iOS: function () {
-			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-		},
-		Opera: function () {
-			return navigator.userAgent.match(/Opera Mini/i);
-		},
-		Windows: function () {
-			return navigator.userAgent.match(/IEMobile/i);
-		},
-		any: function () {
-			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-		}
-	};
+  $('#artstation').click(function() {
+    location.href = 'https://www.artstation.com/annsparrow';
+  });
 
-	var contentWayPoint = function () {
-		var i = 0;
-		$('.animate-box').waypoint(function (direction) {
+  var contentWayPoint = function() {
+    var i = 0;
+    $('.animate-box').waypoint(
+      function(direction) {
+        if (
+          direction === 'down' &&
+          !$(this.element).hasClass('animated-fast')
+        ) {
+          i++;
 
-			if (direction === 'down' && !$(this.element).hasClass('animated-fast')) {
+          $(this.element).addClass('item-animate');
+          setTimeout(function() {
+            $('body .animate-box.item-animate').each(function(k) {
+              var el = $(this);
+              setTimeout(
+                function() {
+                  var effect = el.data('animate-effect');
+                  if (effect === 'fadeIn') {
+                    el.addClass('fadeIn animated-fast');
+                  } else if (effect === 'fadeInLeft') {
+                    el.addClass('fadeInLeft animated-fast');
+                  } else if (effect === 'fadeInRight') {
+                    el.addClass('fadeInRight animated-fast');
+                  } else {
+                    el.addClass('fadeInUp animated-fast');
+                  }
 
-				i++;
+                  el.removeClass('item-animate');
+                },
+                k * 100,
+                'easeInOutExpo'
+              );
+            });
+          }, 50);
+        }
+      },
+      { offset: '85%' }
+    );
+  };
 
-				$(this.element).addClass('item-animate');
-				setTimeout(function () {
+  var contentWayPoint = function() {
+    var i = 0;
+    $('.animate-box').waypoint(
+      function(direction) {
+        if (
+          direction === 'down' &&
+          !$(this.element).hasClass('animated-fast')
+        ) {
+          i++;
 
-					$('body .animate-box.item-animate').each(function (k) {
-						var el = $(this);
-						setTimeout(function () {
-							var effect = el.data('animate-effect');
-							if (effect === 'fadeIn') {
-								el.addClass('fadeIn animated-fast');
-							} else if (effect === 'fadeInLeft') {
-								el.addClass('fadeInLeft animated-fast');
-							} else if (effect === 'fadeInRight') {
-								el.addClass('fadeInRight animated-fast');
-							} else {
-								el.addClass('fadeInUp animated-fast');
-							}
+          $(this.element).addClass('item-animate');
+          setTimeout(function() {
+            $('body .animate-box.item-animate').each(function(k) {
+              var el = $(this);
+              setTimeout(
+                function() {
+                  var effect = el.data('animate-effect');
+                  if (effect === 'fadeIn') {
+                    el.addClass('fadeIn animated-fast');
+                  } else if (effect === 'fadeInLeft') {
+                    el.addClass('fadeInLeft animated-fast');
+                  } else if (effect === 'fadeInRight') {
+                    el.addClass('fadeInRight animated-fast');
+                  } else {
+                    el.addClass('fadeInUp animated-fast');
+                  }
 
-							el.removeClass('item-animate');
-						}, k * 100, 'easeInOutExpo');
-					});
+                  el.removeClass('item-animate');
+                },
+                k * 100,
+                'easeInOutExpo'
+              );
+            });
+          }, 50);
+        }
+      },
+      { offset: '85%' }
+    );
+  };
 
-				}, 50);
+  var goToTop = function() {
+    $('.js-gotop').on('click', function(event) {
+      event.preventDefault();
 
-			}
+      $('html, body').animate(
+        {
+          scrollTop: $('html').offset().top
+        },
+        500,
+        'easeInOutExpo'
+      );
 
-		}, { offset: '85%' });
-	};
+      return false;
+    });
 
+    $(window).scroll(function() {
+      var $win = $(window);
+      if ($win.scrollTop() > 200) {
+        $('.js-top').addClass('active');
+      } else {
+        $('.js-top').removeClass('active');
+      }
+    });
+  };
 
+  var skillsWayPoint = function() {
+    if ($('#my_portfolio-skills').length > 0) {
+      $('#my_portfolio-skills').waypoint(
+        function(direction) {
+          if (direction === 'down' && !$(this.element).hasClass('animated')) {
+            setTimeout(pieChart, 400);
+            $(this.element).addClass('animated');
+          }
+        },
+        { offset: '90%' }
+      );
+    }
+  };
 
-	var contentWayPoint = function () {
-		var i = 0;
-		$('.animate-box').waypoint(function (direction) {
+  // Loading page
 
-			if (direction === 'down' && !$(this.element).hasClass('animated-fast')) {
+  var loaderPage = function() {
+    $('.my_portfolio-loader').fadeOut('slow');
+  };
 
-				i++;
-
-				$(this.element).addClass('item-animate');
-				setTimeout(function () {
-
-					$('body .animate-box.item-animate').each(function (k) {
-						var el = $(this);
-						setTimeout(function () {
-							var effect = el.data('animate-effect');
-							if (effect === 'fadeIn') {
-								el.addClass('fadeIn animated-fast');
-							} else if (effect === 'fadeInLeft') {
-								el.addClass('fadeInLeft animated-fast');
-							} else if (effect === 'fadeInRight') {
-								el.addClass('fadeInRight animated-fast');
-							} else {
-								el.addClass('fadeInUp animated-fast');
-							}
-
-							el.removeClass('item-animate');
-						}, k * 100, 'easeInOutExpo');
-					});
-
-				}, 50);
-
-			}
-
-		}, { offset: '85%' });
-	};
-
-
-
-	var goToTop = function () {
-
-		$('.js-gotop').on('click', function (event) {
-
-			event.preventDefault();
-
-			$('html, body').animate({
-				scrollTop: $('html').offset().top
-			}, 500, 'easeInOutExpo');
-
-			return false;
-		});
-
-		$(window).scroll(function () {
-
-			var $win = $(window);
-			if ($win.scrollTop() > 200) {
-				$('.js-top').addClass('active');
-			} else {
-				$('.js-top').removeClass('active');
-			}
-
-		});
-
-	};
-
-	var skillsWayPoint = function () {
-		if ($('#my_portfolio-skills').length > 0) {
-			$('#my_portfolio-skills').waypoint(function (direction) {
-
-				if (direction === 'down' && !$(this.element).hasClass('animated')) {
-					setTimeout(pieChart, 400);
-					$(this.element).addClass('animated');
-				}
-			}, { offset: '90%' });
-		}
-
-	};
-
-
-	// Loading page
-
-	var loaderPage = function () {
-		$(".my_portfolio-loader").fadeOut("slow");
-	};
-
-	$(function () {
-		contentWayPoint();
-		goToTop();
-		loaderPage();
-		skillsWayPoint();
-
-	});
-
-
-}());
+  $(function() {
+    contentWayPoint();
+    goToTop();
+    loaderPage();
+    skillsWayPoint();
+  });
+})();
